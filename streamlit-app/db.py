a324910eb32d44dfaa086742939f821de2ff9c1f
DWandless <drew.wandless@dxc.com>
2025-10-27 15:48:22 +0000

    changed to st.secrets from .env

--- streamlit-app/db.py
+++ streamlit-app/db.py
@@ -4,11 +4,15 @@ from dotenv import load_dotenv
 from supabase import create_client
 from supabase.lib.client_options import ClientOptions
 import httpx
+import streamlit as st
 
 # Load env and create the client once
-load_dotenv()
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
+# load_dotenv()
+# url = os.getenv("SUPABASE_URL")
+# key = os.getenv("SUPABASE_KEY")
+
+url = st.secrets("SUPABASE_URL")
+key = st.secrets("SUPABASE_KEY")
 
 if not url or not key:
     raise RuntimeError("SUPABASE_URL or SUPABASE_KEY not set in environment")
